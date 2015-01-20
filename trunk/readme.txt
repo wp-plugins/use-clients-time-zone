61f519c8931848593db117964a0d1f1fb6df17b2
drmikegreen <drmikegreen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-20 17:20:39 +0000

    Recommit Version 1.1.1 to add '(requires PHP 5 >= 5.1.0)' to Readme and header.
    git-svn-id: https://plugins.svn.wordpress.org/use-clients-time-zone/tags/1.1.1@1071826 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/readme.txt
@@ -0,0 +1,61 @@
+=== Use Client's Time Zone ===
+Contributors: drmikegreen
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=XR9J849YUCJ3A
+Tags: Time zone, default time zone, client's time zone,
+Requires at least: 1.5
+Tested up to: 4.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Sets the default time zone for PHP scripts to that of the client.
+
+== Description ==
+
+This plugin retrieves the time zone of the client using "Automatic Timezone Detection Using JavaScript" (http://pellepim.bitbucket.org/jstz/) the first time that a client accesses the site, sets a cookie containing the client's time zone on the client, and reloads the page. This cookie is then read by PHP and the date_default_timezone_set() function is used to set the default time zone for PHP scripts. Since it runs just after all of the plugins are loaded, it changes the time zone for all of the PHP that follows. It is useful in cases where one wants users to see certain posts on certain days. E.g., if the post slug contains the day number, like "xyz-daynr," then the post can be specified by "$slug = 'xyz'.date(j);" in a theme template. The cookie does not expire for ten years, but is reset if the user changes time zones. So, unless the user deletes cookies or moves the page reload only occurs once every ten years.
+
+Copyright 2012-2015 M.D. Green, SaeSolved:: LLC
+
+== License ==
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+== Installation ==
+
+1. Extract the use-clients-timezone folder and place in the wp-content/plugins folder.
+
+1. Enable the plugin.
+
+1. Set the fallback time zone under Settings->Use Client's Timezone. (The time zone to use if the script fails to get the client's time zone.)
+
+== Frequently Asked Questions ==
+
+= Where can I see a working example of this plugin? =
+
+http://prayercentral.net/devotionals/a-word-for-today/.
+
+http://church-savior.com/todaysbiblereadings/.
+
+== Screenshots ==
+
+1. Settings page.
+
+== Upgrade Notice ==
+
+= 1.1 =
+Method for obtaining client time zone changed from attempting to retrieve time zone of IP to setting a cookie containing the clients time zone, then reading that cookie.
+
+= 1.0 =
+Original release.
+
+== Changelog ==
+
+= 1.1 =
+Method for obtaining client time zone changed from attempting to retrieve time zone of IP to setting a cookie containing the clients time zone, then reading that cookie.
+
+= 1.0 =
+Original release.
---
+++ trunk/use-clients-timezone-class.inc.php
@@ -0,0 +1,87 @@
+<?php
+
+class UseClientsTimezone {
+	/* Sets the default time zone for PHP scripts to that of the client.
+	 * A cookie containing the client's time zone is set on the client the first time that client accesses the site and the page is reloaded.
+	 * This cookie is then read by the PHP of this script and used to set the default time zone for PHP scripts.
+	 */
+
+	protected $doc_root				= '';
+	protected $fallback_timezone	= 'GMT';
+	protected $plugin_path			= '';
+
+	public function __construct () {
+		$this->doc_root				= 'http://'.$_SERVER['SERVER_NAME'];
+		$this->plugin_path			= plugins_url('/use-clients-timezone/');
+		$this->fallback_timezone	= get_option('use_clients_timezone_fallback_timezone');
+		if (!$this->fallback_timezone) {
+			$this->fallback_timezone = 'GMT';
+			update_option('use_clients_timezone_fallback_timezone', $this->fallback_timezone);
+		}
+		add_action('admin_init', array(&$this, 'initialize_admin'));
+		add_action('admin_menu', array(&$this, 'admin_add_page'));
+	}
+
+	public function enqueueScripts () {
+		wp_register_script('jstimezonedetect', 'https://cdnjs.cloudflare.com/ajax/libs/jstimezonedetect/1.0.4/jstz.min.js'); // Automatic Timezone Detection Using JavaScript: http://pellepim.bitbucket.org/jstz/
+		wp_register_script('jquery-cookie', plugins_url('js/jquery-cookie-1.4.1/jquery.cookie.js', __FILE__), array('jquery')); // jquery-cookie https://github.com/carhartl/jquery-cookie/tree/v1.4.1
+		wp_register_script('set_uct_timezone_cookie', plugins_url('js/set_uct_timezone_cookie.js', __FILE__), array('jquery', 'jquery-cookie', 'jstimezonedetect'));
+		wp_enqueue_script('jstimezonedetect');
+		wp_enqueue_script('jquery-cookie');
+		wp_enqueue_script('set_uct_timezone_cookie');
+	} 
+	
+	public function initialize_admin () {
+		if (function_exists('register_setting')) {
+			$page_for_settings		= 'use_clients_timezone_plugin';
+			$section_for_settings	= 'use_clients_timezone_section';
+			add_settings_section($section_for_settings, 'Use Client&#039;s Time Zone Settings', array(&$this, 'use_clients_timezone_section_heading'), $page_for_settings);
+			add_settings_field('use_clients_timezone_fallback_timezone_id', 'Fallback time zone', array(&$this, 'use_clients_timezone_setting_values'), $page_for_settings, $section_for_settings);
+			register_setting('use_clients_timezone_settings', 'use_clients_timezone_fallback_timezone', 'wp_filter_nohtml_kses');
+		}
+	}
+
+	public function use_clients_timezone_section_heading () {
+		echo 'Enter the time zone to be used if the plugin is not able to retrieve the client&#039;s time zone (<a href="http://php.net/manual/en/timezones.php" target="_blank">list of supported timezones</a>).<br />Do not enclose entry in quotation marks, either single (&#039;) or double (&quot;).';
+	}
+
+	public function use_clients_timezone_setting_values () {
+		$use_clients_timezone_fallback_timezone = get_option('use_clients_timezone_fallback_timezone');
+		echo '<input id="use_clients_timezone_fallback_timezone_input" name="use_clients_timezone_fallback_timezone" size="35" type="text" value="'.$use_clients_timezone_fallback_timezone.'" />';
+	}
+
+	public function admin_add_page() {
+		add_options_page('Use Client&#039;s Time Zone Settings', 'Use Client&#039;s Time Zone', 'manage_options', 'use_clients_timezone_plugin', array(&$this, 'draw_options_page'));
+	}
+
+	public function draw_options_page () {
+		echo '<div><h2>Use Client&#039;s Time Zone Options</h2>';
+		echo '<form method="post" action="options.php">';
+		settings_fields('use_clients_timezone_settings');
+		do_settings_sections('use_clients_timezone_plugin');
+		echo '<p><input name="Submit" type="submit" value="';
+		esc_attr_e('Save Changes');
+		echo '" /></p>';
+		echo '</form></div>';
+		echo '<div style="width: 50%; margin-top: 25px;">If you find this plugin of value, please contribute to the cost of its developement:<div style="margin: auto; text-align: center"><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="RNEZMH62QWKQE">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form></div></div>';
+	}
+
+	public function setTimezone () {
+		$uct_cookie_name = 'wordpress_useclientstimezone_timezone';
+		if(isset($_COOKIE[$uct_cookie_name])) {
+			if (!date_default_timezone_set($_COOKIE[$uct_cookie_name])) {
+				date_default_timezone_set($this->fallback_timezone);
+			}
+		} else {
+			date_default_timezone_set($this->fallback_timezone);
+		}
+	}
+
+}
+
+?>
