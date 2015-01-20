feadd41c8fbae74729fca33476c24797b14c460b
drmikegreen <drmikegreen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-20 00:55:14 +0000

    Version 1.1. Changed method for obtaining client time zone from attempting to retrieve time zone of IP to setting a cookie containing the clients time zone, then reading that cookie.
    git-svn-id: https://plugins.svn.wordpress.org/use-clients-time-zone/tags/1.1@1071418 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- use-clients-timezone.php
+++ use-clients-timezone.php
@@ -1,18 +1,18 @@
 <?php
 /**
  * @package Use Client's Time Zone
- * @version 1.0
+ * @version 1.1
  */
 
 /*
 Plugin Name: Use Client's Time Zone
 Plugin URI:
-Description: Retrieves the time zone of the IP number of the client using http://api.easyjquery.com/ips/. It then uses the PHP date_default_timezone_set() function to set the default time zone. Since it runs just after all of the plugins are loaded, it changes the time zone for all of the PHP that follows. It is useful in cases where one wants users to see certain posts on certain days. E.g., if the post slug contains the day number, like "xyz-daynr," then the post can be specified by "$slug = 'xyz'.date(j);" in a theme template.
-Version: 1.0
+Description: The first time that a client accesses the site the time zone of the client is retrieved using "Automatic Timezone Detection Using JavaScript" (http://pellepim.bitbucket.org/jstz/), a cookie containing the client's time zone is set on the client, and the page is reloaded. This cookie is then read by the PHP of this script which uses the date_default_timezone_set() function to set the default time zone for PHP scripts. Since it runs just after all of the plugins are loaded, it changes the time zone for all of the PHP that follows. It is useful in cases where one wants users to see certain posts on certain days. E.g., if the post slug contains the day number, like "xyz-daynr," then the post can be specified by "$slug = 'xyz'.date(j);" in a theme template. The cookie does not expire for ten years, but is reset if the user changes time zones. So, unless the user deletes cookies or moves the page reload only occurs once every ten years.
+Version: 1.1
 Author: M.D. Green
 Author URI: http://saesolved.com/
 
-Copyright 2012 M.D. Green, SaeSolved:: LLC
+Copyright 2012-2015 M.D. Green, SaeSolved:: LLC
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the
 Free Software Foundation; either version 2 of the License, or (at your option) any later version.
